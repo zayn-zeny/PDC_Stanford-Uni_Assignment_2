35e6b9f1c39b905c36a4b47ca67b8444dc61d53d
root <root@ZAYN.localdomain>
2026-09-17 12:30:00 +0500

    Implement contiguous block spatial decomposition to demonstrate load imbalance in timing

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <thread>
+#include <cstdlib>
 
 #include "CycleTimer.h"
 
@@ -29,13 +30,20 @@ extern void mandelbrotSerial(
 // Thread entrypoint.
 void workerThreadStart(WorkerArgs * const args) {
 
-    // TODO FOR CS149 STUDENTS: Implement the body of the worker
-    // thread here. Each thread should make a call to mandelbrotSerial()
-    // to compute a part of the output image.  For example, in a
-    // program that uses two threads, thread 0 could compute the top
-    // half of the image and thread 1 could compute the bottom half.
+    double startTime = CycleTimer::currentSeconds();
 
-    printf("Hello world from thread %d\n", args->threadId);
+    int rowsPerThread = args->height / args->numThreads;
+    int startRow = args->threadId * rowsPerThread;
+    int numRows = (args->threadId == args->numThreads - 1) ? (args->height - startRow) : rowsPerThread;
+
+    mandelbrotSerial(args->x0, args->y0, args->x1, args->y1,
+                     args->width, args->height,
+                     startRow, numRows,
+                     args->maxIterations,
+                     args->output);
+
+    double endTime = CycleTimer::currentSeconds();
+    printf("Thread %d time: %.3f ms\n", args->threadId, (endTime - startTime) * 1000);
 }
 
 //
