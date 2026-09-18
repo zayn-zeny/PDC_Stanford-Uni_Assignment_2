fc2b08eef0a80c4d21c2ccfabab8a15c6796d8ab
root <root@ZAYN.localdomain>
2026-09-18 12:00:00 +0500

    Complete worst-case SIMD initialization for prog4_sqrt

--- prog4_sqrt/main.cpp
+++ prog4_sqrt/main.cpp
@@ -29,12 +29,11 @@ int main() {
 
     for (unsigned int i=0; i<N; i++)
     {
-        // TODO: CS149 students.  Attempt to change the values in the
-        // array here to meet the instructions in the handout: we want
-        // to you generate best and worse-case speedups
-        
-        // starter code populates array with random input values
-        values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+        // worst-case speedup (ISPC AVX2 is 8-wide)
+        if (i % 8 == 0)
+            values[i] = 2.999f;
+        else
+            values[i] = 1.0f;
     }
 
     // generate a gold version to check results
